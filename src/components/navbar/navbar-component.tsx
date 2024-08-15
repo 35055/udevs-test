cf835f7ce7b21b3d16ec1e4638c505b205d935f9
Murat <mur@sd.com>
2024-08-15 11:50:26 +0500

    add action-icon;

--- src/components/navbar/navbar-component.tsx
+++ src/components/navbar/navbar-component.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Flex } from "@mantine/core";
+import { ActionIcon, Avatar, Flex } from "@mantine/core";
 import avatar from "../../assets/avatar.jpg";
 import {
   IconAlignBoxBottomLeftFilled,
@@ -8,9 +8,13 @@ import {
 export const NavbarComponent = () => {
   return (
     <Flex h="100%" direction="column" justify="space-between" align="center">
-      <IconAlignBoxBottomLeftFilled color="#1c7ed6" size={40} />
+      <ActionIcon w={40} h={40}>
+        <IconAlignBoxBottomLeftFilled color="#1c7ed6" size={40} />
+      </ActionIcon>
       <Flex direction="column" align="center" gap="xs">
-        <IconSettings size={30} color="#868e96" />
+        <ActionIcon w={30} h={30}>
+          <IconSettings size={30} color="#868e96" />
+        </ActionIcon>
         <Avatar radius="xl" src={avatar} alt="it's me" />
       </Flex>
     </Flex>
